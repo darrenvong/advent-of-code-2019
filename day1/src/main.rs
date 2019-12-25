ea1ef3b5aac6abecae71af7291ba46d4149e127a
Darren Vong <d.vongy@gmail.com>
2019-12-25 01:51:10 +0000

    Completed day 1 and refactored repo into using workspace

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,22 +1,44 @@
+use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::fs::File;
+use helpers;
+
+fn calculate_fuel_requirements<F>(
+    reader: &mut BufReader<File>,
+    get_fuel_requirement: F) -> isize
+    where F: Fn(Result<String, std::io::Error>) -> isize
+{
+    reader.lines()
+        .map(get_fuel_requirement)
+        .sum()
+}
 
 fn main() {
-    let f = File::open("input.txt").unwrap();
-    let reader = BufReader::new(f);
+    let mut reader = helpers::read_puzzle_input("day1/input.txt");
 
-    let part1: isize = reader.lines()
-        .map(|mass| {
-            let mass = mass.unwrap();
-            let mass = mass.parse::<isize>().unwrap();
-            mass / 3 - 2
-        })
-        .sum();
+    let part1 = calculate_fuel_requirements(&mut reader, |mass| {
+        let mass = mass.unwrap();
+        let mass = mass.parse::<isize>().unwrap();
+        mass / 3 - 2
+    });
+
+    helpers::reset_reader(&mut reader);
+
+    let part2 = calculate_fuel_requirements(&mut reader, |mass| {
+        let mut mass = mass.unwrap().parse::<isize>().unwrap();
+        let mut total_mass = 0;
+
+        while mass >= 0 {
+            mass = mass / 3 - 2;
+            total_mass = if mass > 0 { total_mass + mass } else { total_mass };
+        }
+        total_mass
+    });
     
     let marker = "-".repeat(15);
     println!("{} Part 1 {}", marker, marker);
     println!("The sum of fuel requirements: {}", part1);
 
     println!("\n{} Part 2 {}", marker, marker);
+    println!("The sum of fuel requirements: {}", part2);
 }
---
+++ helpers/src/lib.rs
@@ -0,0 +1,15 @@
+use std::io::{BufReader, Seek, SeekFrom};
+use std::fs::File;
+
+pub fn read_puzzle_input(path: &str) -> BufReader<File> {
+    let f = File::open(path).unwrap();
+    let reader = BufReader::new(f);
+
+    reader
+}
+
+/// Reset the buffer reader position so its content can be
+/// read for another time.
+pub fn reset_reader(reader: &mut BufReader<File>) {
+    reader.seek(SeekFrom::Start(0)).unwrap();
+}
